263c09c4fb2f4ffd19b4472a60995d161e49bf90
Nicxv <nico0valenzuelacl@gmail.com>
2024-09-12 19:16:03 -0300

    Reapply "render"
    This reverts commit a63e5d9d32284cec946d4accee12dfb68060e3e0.

--- invierteenterrenos/settings.py
+++ invierteenterrenos/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bnwtngnmichv3s)@sq-n-!!y22t@(ts6!+e!eaksin#c#+zzxv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['appinvierteenterrenos.onrender.com']
 
 
 # Application definition
@@ -116,7 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
