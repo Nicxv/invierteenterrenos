33897f77e6759e5dac50d316b7f7141a43bcf37e
Nicxv <nico0valenzuelacl@gmail.com>
2024-09-12 18:46:11 -0300

    Revert "5to"
    This reverts commit 285d2b58a5eb4d11d63665b21c0f1d707a8d0949.

--- invierteenterrenos/settings.py
+++ invierteenterrenos/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bnwtngnmichv3s)@sq-n-!!y22t@(ts6!+e!eaksin#c#+zzxv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
+
 
 # Application definition
 
@@ -115,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
